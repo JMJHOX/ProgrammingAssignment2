ffc60d99d872bf8d957d47632fd29202089a128d
JMJHOX <40309454+JMJHOX@users.noreply.github.com>
2020-08-22 18:34:45 -0500

    Solution for assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+#*The function "makeCacheMatrix" creates a object that can set a matrix
+#and its inverse separately
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x=matrix()) {
+  m <- NULL
+  setMatrix <- function(y) {
+    x <<- y
+    m <<- NULL}
 
+getMatrix <- function()x
+getInverse <- function()m 
+setInverse <- function(solve)m <<-solve
+list(getMatrix = getMatrix,setMatrix=setMatrix,getInverse=getInverse
+     ,setInverse=setInverse)
 }
 
 
-## Write a short comment describing this function
-
+#"cacheSolve" determines if the matrix has a inverse. If not,will return 
+#a new inverse and save it as cache for future calculations. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      if(!is.null(x$getInverse()) && !is.na(x$getInverse()))
+        {
+        print("Getting cached inverse")
+        return(x$getInverse())
+        }
+        
+      else if(is.null(x$getInverse()) && !is.null(x$getMatrix()))
+        { 
+        newInv <- solve(x$getMatrix()) 
+          print("The new inverse is:")
+          x$setInverse(newInv)
+          return (newInv)
+        }
+        else
+        {
+          print("error: Please enter a valid Matrix to determine its inverse")
+        }
+        
 }
